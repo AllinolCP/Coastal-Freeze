fbd2b52e9b09ee743321946f2404da95ca916850
AllinolCP <56921534+AllinolCP@users.noreply.github.com>
2020-12-25 22:28:36 +0700

    1.3.6 Added DarkMode, and saving mute

--- main.js
+++ main.js
@@ -4,58 +4,69 @@ const {
     BrowserWindow,
     Menu,
     MenuItem,
-	ipcMain
+    ipcMain,
+    nativeTheme
 } = require('electron')
 
-const { autoUpdater } = require('electron-updater');
+const {
+    autoUpdater
+} = require('electron-updater');
 
 const path = require('path')
 
 
 let pluginName
 switch (process.platform) {
-	case 'win32':
-		switch(process.arch){
-			case 'ia32':
-				pluginName = 'flash/pepflashplayer32_32_0_0_303.dll'
-				break
-			case 'x32':
-				pluginName = 'flash/pepflashplayer32_32_0_0_303.dll'
-				break
-			case 'x64':
-				pluginName = 'flash/pepflashplayer64_32_0_0_303.dll'
-				break
-		}
-		break
-	case 'darwin':
-		pluginName = 'flash/PepperFlashPlayer.plugin'
-		break
-	case 'linux':
-		pluginName = 'flash/libpepflashplayer.so'
-		break
+case 'win32':
+    switch (process.arch) {
+    case 'ia32':
+        pluginName = 'flash/pepflashplayer32_32_0_0_303.dll'
+        break
+    case 'x32':
+        pluginName = 'flash/pepflashplayer32_32_0_0_303.dll'
+        break
+    case 'x64':
+        pluginName = 'flash/pepflashplayer64_32_0_0_303.dll'
+        break
+    }
+    break
+case 'darwin':
+    pluginName = 'flash/PepperFlashPlayer.plugin'
+    break
+case 'linux':
+    pluginName = 'flash/libpepflashplayer.so'
+    break
 }
 app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname, pluginName));
 
 var win
 app.on('ready', () => {
     win = new BrowserWindow({
-		title: "Coastal Freeze",
+        title: "Coastal Freeze",
         webPreferences: {
             plugins: true,
-			nodeIntegration: true
+            nodeIntegration: true
         }
     })
-	makeMenu()
+    makeMenu()
+    ipcMain.on('load:data', (event, mute, theme) => {
+        muted = (mute == 'true');
+		console.log(muted, typeof(muted))
+        nativeTheme.themeSource = theme;
+        win.webContents.audioMuted = muted;
+    });
     win.loadFile('index.html');
-	
-	autoUpdater.checkForUpdatesAndNotify();
-	
+    autoUpdater.checkForUpdatesAndNotify();
     Menu.setApplicationMenu(fsmenu);
 })
 
-const aboutMessage = `Coastal Freeze Client ${app.getVersion()}
+// start of menubar part
+
+const aboutMessage = `Coastal Freeze Client v${app.getVersion()}
 Created by Allinol and Random for use with Coastal Freeze.
 Owners of Coastal Freeze: Fliberjig1 and Snickerdoodle`;
+
+
 
 function makeMenu() { // credits to random
     fsmenu = new Menu();
@@ -78,12 +89,20 @@ function makeMenu() { // credits to random
                     accelerator: 'CmdOrCtrl+F',
                     click: () => {
                         win.setFullScreen(!win.isFullScreen());
+                        win.webContents.send('fullscreen', win.isFullScreen());
                     }
                 },
                 {
                     label: 'Mute Audio (Toggle)',
                     click: () => {
-                        win.webContents.audioMuted = !win.webContents.audioMuted;;
+                        win.webContents.audioMuted = !win.webContents.audioMuted;
+                        win.webContents.send('muted', win.webContents.audioMuted);
+                    }
+                },
+                {
+                    label: 'Dark Mode (Toggle)',
+                    click: () => {
+                        darkMode()
                     }
                 },
                 {
@@ -95,8 +114,7 @@ function makeMenu() { // credits to random
                 }
             ]
         }));
-    }
-    else {
+    } else {
         fsmenu.append(new MenuItem({
             label: 'About',
             click: () => {
@@ -113,12 +131,20 @@ function makeMenu() { // credits to random
             accelerator: 'CmdOrCtrl+F',
             click: () => {
                 win.setFullScreen(!win.isFullScreen());
+                win.webContents.send('fullscreen', win.isFullScreen());
             }
         }));
         fsmenu.append(new MenuItem({
             label: 'Mute Audio (Toggle)',
             click: () => {
                 win.webContents.audioMuted = !win.webContents.audioMuted;
+                win.webContents.send('muted', win.webContents.audioMuted);
+            }
+        }));
+        fsmenu.append(new MenuItem({
+            label: 'Dark Mode (Toggle)',
+            click: () => {
+                darkMode()
             }
         }));
         fsmenu.append(new MenuItem({
@@ -130,27 +156,50 @@ function makeMenu() { // credits to random
         }));
     }
 }
-function clearCache(){
-	windows = BrowserWindow.getAllWindows()[0];
-	const ses = win.webContents.session;
-	ses.clearCache(() => {});
+
+function clearCache() {
+    windows = BrowserWindow.getAllWindows()[0];
+    const ses = win.webContents.session;
+    ses.clearCache(() => {});
 }
+
+function darkMode() {
+    if (nativeTheme.shouldUseDarkColors) {
+        nativeTheme.themeSource = 'light'
+    } else {
+        nativeTheme.themeSource = 'dark'
+    }
+    win.webContents.send('theme', nativeTheme.themeSource);
+    return nativeTheme.shouldUseDarkColors
+}
+
+
+// end of menubar
 
 // Auto update part
 
 autoUpdater.on('update-available', () => {
-  win.webContents.send('update_available');
+    win.webContents.send('update_available');
+});
+
+autoUpdater.on('update-downloaded', () => {
+    win.webContents.send('update-downloaded');
 });
 
 ipcMain.on('app_version', (event) => {
-  event.sender.send('app_version', { version: app.getVersion() });
+    event.sender.send('app_version', {
+        version: app.getVersion()
+    });
+});
+
+ipcMain.on('restart_app', () => {
+    autoUpdater.quitAndInstall();
 });
 
 // end of Auto update part
 
-
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
